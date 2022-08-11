9308a9c73c2128bdc027c31e20580f20cb4663c6
hackwebdev <hackwebdev@gmail.com>
2022-08-11 14:05:40 +0800

    css animated hamburger

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // nav toggle - select button and links
 const navToggle = document.querySelector('#navToggle')
 const nav = document.querySelector('#nav-links')
+const hamburger = document.querySelector('.nav-toggle')
 
 // add event listener
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('nav-open')
+})
+
+hamburger.addEventListener('click', function () {
+  this.classList.toggle('is-active')
 })
